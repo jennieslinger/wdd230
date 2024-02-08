c44771618056dff0368cb5cf7891ea4836ed1e7d
jennieslinger <jennieslinger@gmail.com>
2024-02-08 14:14:53 -0700

    bom

---
+++ lesson05/scripts/bom.js
@@ -0,0 +1,57 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+    if (input != "") {
+        const li = document.createElement('li');
+
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+
+        deleteButton.textContent = '❌';
+
+        li.append(deleteButton);
+
+        list.append(li);
+
+        input.value = '';
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+            input.value = '';
+        });
+    }
+    else {
+        input.focus();
+
+    }
+
+
+
+});
+
+//if (input.value != '') { ??? }
+
+//const li = document.createElement('li');
+
+//const deleteButton = document.createElement('button');
+
+//li.textContent = input.value;
+
+//deleteButton.textContent = '❌';
+
+//li.append(deleteButton);
+
+//list.append(li);
+
+//deleteButton.addEventListener('click', function () {
+//list.removeChild(li);
+//input.focus();
+//});
+
+input.focus();
+
+input.value = '';
