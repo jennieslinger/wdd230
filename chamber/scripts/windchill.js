7c560c7da6515d85ff261e688ae96431c0e4cde6
jennieslinger <jennieslinger@gmail.com>
2024-02-15 20:52:27 -0700

    chamber hero img

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,17 @@
+get the two input values of temperature and wind speed,
+
+
+    check to make sure they meet the specification limits(<= 50°F and > 3.0mph) allowed to officially calculate the wind chill, and
+
+
+either calculate and display the wind chill factor value or display "N/A"(not applicable) if the input values did not meet the requirements.
+
+💡Wind Chill Help
+It is recommended that you isolate the temperature and wind speed values in your HTML document for easy extraction.When you isolate a value, consider using the tag and do not include the unit labels in your spanned content.
+
+The formula to calculate the wind chill factor is 
+where Twc is the wind chill index in Fahrenheit, Ta is the air temperature in Fahrenheit, and v is the wind speed in miles per hour.
+
+Wind Chill Specification Limits: "Wind Chill Temperature is officially defined for temperatures at or below 10 °C (50 °F) and wind speeds above 4.8 kilometers per hour (3.0 mph)." - U.S.National Weather ServiceLinks to an external site.
+
+In the future, we will be getting live temperature and wind speed values from which we can plug into our wind chill function.
