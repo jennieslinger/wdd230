69d4f10ab85e168b95a405e6b72a5309b6f74b12
jennieslinger <jennieslinger@gmail.com>
2024-02-24 09:13:12 -0700

    windchill

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,19 +1,11 @@
-Windchill = 35.74 + (0.6215*temperature)-(35.75*speed)
+var temperature = 39;
+var windspeed = 13;
+var windchill = 35.74 + (0.6215 * temperature) - (35.75 * (windspeed ** 0.16)) + (0.4275 * (temperature) * (windspeed ** 0.16));
 
-get the two input values of temperature and wind speed,
+var windchill = Math.round(windchill);
+document.getElementById("windchill").innerHTML = windchill;
 
+/*get the two input values of temperature and wind speed,
+check to make sure they meet the specification limits(<= 50°F and > 3.0mph) allowed to officially calculate the wind chill, and
 
-    check to make sure they meet the specification limits(<= 50°F and > 3.0mph) allowed to officially calculate the wind chill, and
-
-
-either calculate and display the wind chill factor value or display "N/A"(not applicable) if the input values did not meet the requirements.
-
-💡Wind Chill Help
-It is recommended that you isolate the temperature and wind speed values in your HTML document for easy extraction.When you isolate a value, consider using the tag and do not include the unit labels in your spanned content.
-
-The formula to calculate the wind chill factor is 
-where Twc is the wind chill index in Fahrenheit, Ta is the air temperature in Fahrenheit, and v is the wind speed in miles per hour.
-
-Wind Chill Specification Limits: "Wind Chill Temperature is officially defined for temperatures at or below 10 °C (50 °F) and wind speeds above 4.8 kilometers per hour (3.0 mph)." - U.S.National Weather ServiceLinks to an external site.
-
-In the future, we will be getting live temperature and wind speed values from which we can plug into our wind chill function.
+Wind Chill Specification Limits: "Wind Chill Temperature is officially defined for temperatures at or below 10 °C (50 °F) and wind speeds above 4.8 kilometers per hour (3.0 mph)." - U.S.National Weather ServiceLinks to an external site.*/
