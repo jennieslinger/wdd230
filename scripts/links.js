a2374299da3f32a7ac19f1d55a987d9d603601be
jennieslinger <jennieslinger@gmail.com>
2024-03-19 12:40:56 -0700

    links

--- scripts/links.js
+++ scripts/links.js
@@ -2,18 +2,54 @@ const baseURL = "https://jennieslinger.github.io/wdd230";
 
 const linksURL = "https://jennieslinger.github.io/wdd230/data/links.json";
 
-async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    //console.log(data.lessons);
-    displayLinks(data);
+
+const cards = document.querySelector('.links');
+// const ccc = document.querySelector('.card');
+// console.log(ccc);
+console.log(cards);
+
+function displayLinks(weeks) {
+    weeks.forEach(week => {
+        let article = document.createElement("article");
+
+        let weekEl = document.createElement('h5');
+        weekEl.textContent = `${week.lesson}: `
+
+        console.log(week.links)
+
+        week.links.forEach(link => {
+            let linkEl = document.createElement('a');
+            linkEl.innerHTML = `${link.title} | `;
+            linkEl.href = link.url;
+            weekEl.appendChild(linkEl);
+
+
+            // url.textContent = `${link.url}`;
+            // title.textContent = `${link.title}`;
+        });
+
+        article.appendChild(weekEl);
+        // article.appendChild(link);
+
+        cards.appendChild(article)
+
+    });
 }
 
 getLinks();
 
-function displayLinks(weeks) {
-    data.forEach(week => {
-        let article = document.createElement("article");
+async function getLinks() {
 
-    });
+    try {
+        const response = await fetch(linksURL);
+        if (!response.ok) {
+            throw new Error("Could not fetch data");
+        }
+        const data = await response.json();
+        console.log(data);
+        displayLinks(data.lessons);
+    }
+    catch (error) {
+        console.error(error);
+    }
 }
