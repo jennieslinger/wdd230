e5f1aef05904ae5ed60885a3e316bc89239262dc
jennieslinger <jennieslinger@gmail.com>
2024-03-19 19:51:11 -0700

    directory

---
+++ chamber/scripts/members.js
@@ -0,0 +1,42 @@
+const url = "https://jennieslinger.io.github/WDD230/chamber/data/members.json";
+
+const cards = document.querySelector("#cards");
+
+const displayProphets = (prophets) => {
+    prophets.forEach(prophet => {
+        let section = document.createElement("section");
+
+        let fullName = document.createElement("h2");
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+        let birthdate = document.createElement("p");
+        birthdate.textContent = `Birthdate: ${prophet.birthdate}`;
+
+        let birthplace = document.createElement('p');
+        birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
+        
+        let portrait = document.createElement("img");
+        portrait.setAttribute("src", prophet.imageurl);
+        portrait.setAttribute("alt", fullName);
+        portrait.setAttribute("loading", "lazy");
+        portrait.setAttribute("width", 200);
+        portrait.setAttribute("height", 325);
+
+        section.appendChild(fullName);
+        section.appendChild(birthdate);
+        section.appendChild(birthplace);
+        section.appendChild(portrait);
+
+
+        cardsElement.appendChild(section)
+    });
+}
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+
+getProphetData();
